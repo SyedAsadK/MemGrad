a9e1cd5a16efce8caeb02b579a268086a7891af5
SyedAsadK <syedasad1610@gmail.com>
2026-08-30 22:00:55 +0530

    agile

--- agilecoder/agilecoder/cli.py
+++ agilecoder/agilecoder/cli.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 from agilecoder.run_api import run_task
+
+
 def main():
     parser = argparse.ArgumentParser(description='argparse')
     parser.add_argument('--config', type=str, default="Agile",
@@ -12,6 +14,10 @@ def main():
     parser.add_argument('--name', type=str, default="Gomoku",
                         help="Name of software, your software will be generated in WareHouse/name_org_timestamp")
     parser.add_argument('--model', type=str, default="GPT_3_5_AZURE",
-                        help="GPT Model, choose from {'GPT_3_5_TURBO','GPT_4','GPT_4_32K', 'GPT_3_5_AZURE','CLAUDE'}")
+                        help="GPT Model, choose from {'GPT_3_5_TURBO','GPT_4','GPT_4_32K', 'GPT_3_5_AZURE','CLAUDE','OLLAMA'}")
     args = parser.parse_args()
-    run_task(args)                                           
+    run_task(args)
+
+
+if __name__ == "__main__":
+    main()                                           
